8074d001faf65876586673174247e47997543025
Paul <paul.stenka@gmail.com>
2014-11-24 18:51:32 +0100

    +

--- app/models/SimpleChat.java
+++ app/models/SimpleChat.java
@@ -3,31 +3,46 @@ package models;
 /**
  * Created by cookie on 22.11.14.
  */
+import jdk.nashorn.internal.parser.JSONParser;
 import play.mvc.*;
 import play.libs.*;
 import play.libs.F.*;
-import org.json.*;
-import scala.util.parsing.json.JSONObject;
-import scala.util.parsing.json.JSONObject$;
-
+import org.json.JSONObject;
 import java.util.*;
+
+import static play.mvc.Controller.request;
 
 public class SimpleChat{
 
     // collect all websockets here
     private static List<WebSocket.Out<String>> connections = new ArrayList<WebSocket.Out<String>>();
-
+    public static List<String> users;
     public static void start(WebSocket.In<String> in, WebSocket.Out<String> out){
 
-        //JSONObject obj = new JSONObject("asd");
+        for(int i=0; i<connections.size();i++) {
+            System.out.println(connections.get(i) +"");
+      //  play.core.j.JavaWebSocket.webSocketWrapper(connections);
+        }
 
         connections.add(out);
 
-        in.onMessage(new Callback<String>(){
-            public void invoke(String event){
-                SimpleChat.notifyAll(event);
+        in.onMessage(new Callback<String>() {
+            @Override
+            public void invoke(String s) throws Throwable {
+                JSONObject jsonObj = new JSONObject(s);
+
+                SimpleChat.notifyAll(s);
+
+
             }
         });
+
+
+ //       in.onMessage(new Callback<String>(){
+ //           public void invoke(String event){
+  //              SimpleChat.notifyAll(event);
+   //         }
+   //     });
 
         in.onClose(new Callback0(){
             public void invoke(){
@@ -38,7 +53,9 @@ public class SimpleChat{
 
     // Iterate connection list and write incoming message
     public static void notifyAll(String message){
+
         for (WebSocket.Out<String> out : connections) {
+
             out.write(message);
         }
     }
